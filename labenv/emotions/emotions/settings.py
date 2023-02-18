d9c3153772b8db6d071cf795ace13e7d3bece965
Vera <veray@sfu.ca>
2023-02-18 12:10:20 -0800

    Added username/password to file

--- labenv/emotions/emotions/settings.py
+++ labenv/emotions/emotions/settings.py
@@ -19,6 +19,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+#### Username: admin
+#### Password: rosie2023
+
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
